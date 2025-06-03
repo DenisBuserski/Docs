bcd42a31b435222e27139d6cf108d54d78517722
Denis Buserski <denis.buserski@protonmail.com>
2025-06-03 16:14:22 +0300

    Update locustfile.py

--- Locust_testing/locustfile.py
+++ Locust_testing/locustfile.py
@@ -1,9 +1,21 @@
 from locust import HttpUser, task, between
+import random
 
-class QuickTestUser(HttpUser):
-    wait_time = between(1, 3)
+class ProductTestUser(HttpUser):
+    wait_time = between(1, 2)  # Simulate think time between requests
 
     @task
     def hello(self):
         self.client.get("/hello/")
+
+    @task
+    def get_product_by_id(self):
+        product_id = random.randint(1, 1000)  # simulate different product IDs
+        with self.client.get(f"/product/get/{product_id}", name="/product/get/:id", catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure(f"Failed to fetch product {product_id}: {response.status_code}")
+            else:
+                response.success()
+
+
 
