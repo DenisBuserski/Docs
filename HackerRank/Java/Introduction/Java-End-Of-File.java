8bbc1ff3f128b63cfd08f1cb4ead668477858f13
Denis Buserski <denis.buserski@protonmail.com>
2025-08-02 20:56:54 +0300

    Create Java-End-Of-File.java

---
+++ HackerRank/Java/Introduction/Java-End-Of-File.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 1;
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            System.out.println(count + " " + input);
+            count++;
+        }
+    }
+}
