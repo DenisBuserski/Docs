8025605c03adc9878ce2f4dcd4d42d68336cf18c
Denis Buserski <denis.buserski@protonmail.com>
2025-08-02 20:39:01 +0300

    Update and rename JavaStdinAndStdoutII.java to P4_Java-Stdin-And-Stdout-II.java

--- HackerRank/Java/P4_Java-Stdin-And-Stdout-II.java
+++ HackerRank/Java/P4_Java-Stdin-And-Stdout-II.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int i = Integer.parseInt(scanner.nextLine());
         double d = Double.parseDouble(scanner.nextLine());
         String s = scanner.nextLine();
-        
+
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
