d91cb7edaf2d5b9a7891d619ac03ae80616cd560
Denis Buserski <denis.buserski@protonmail.com>
2025-08-02 20:37:04 +0300

    Update and rename JavaIfElse.java to P3_Java-If-Else.java

--- HackerRank/Java/P3_Java-If-Else.java
+++ HackerRank/Java/P3_Java-If-Else.java
@@ -1,18 +1,12 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
-public class Solution {
+public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        
+
         if (N % 2 != 0) {
             System.out.println("Weird");
         } else if (N % 2 == 0 && N >= 2 && N <= 5) {
