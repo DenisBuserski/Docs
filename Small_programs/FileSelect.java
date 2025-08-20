b05f8818a9ad1355418fae544968b7bfaba9ddeb
Denis Buserski <denis.buserski@protonmail.com>
2025-08-20 23:47:34 +0300

    Create FileSelect.java

---
+++ Small_programs/FileSelect.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class FileSelect extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+        new FileSelect();
+
+    }
+
+    JButton button;
+
+    FileSelect() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Select File");
+        button.addActionListener(this);
+        this.add(button);
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("/home/user/Desktop")); // Change default folder; Here we put the file location
+            // int response = fileChooser.showOpenDialog(null); // Select File to open; Returns int value
+            int response = fileChooser.showSaveDialog(null); // Select File to save
+
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
+        }
+    }
+}
