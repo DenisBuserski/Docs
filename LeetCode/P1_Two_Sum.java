fd27520c7270e0372606e17e08994bcb986b7026
Denis Buserski <denis.buserski@protonmail.com>
2025-08-23 12:11:42 +0300

    Update P1_Two_Sum.java

--- LeetCode/P1_Two_Sum.java
+++ LeetCode/P1_Two_Sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-                for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int current = nums[i];
             for (int j = i + 1; j < nums.length; j++) {
                 int next = nums[j];
