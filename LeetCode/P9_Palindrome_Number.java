52b9859eb07bdb13e60f265a0dcffc42dd7a398c
Denis Buserski <denis.buserski@protonmail.com>
2025-08-23 12:37:30 +0300

    Create P9_Palindrome_Number.java

---
+++ LeetCode/P9_Palindrome_Number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        int reverse = 0;
+        int temp = x;
+        while (temp != 0) {
+            int lastDigit = temp % 10;
+            reverse = (reverse * 10) + lastDigit;
+            temp = temp / 10;
+        }
+
+        if (x == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
